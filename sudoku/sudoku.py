516db31066fcca80a0bfb9db10216dc2cd388645
Malaya2184 <Malaya2184@users.noreply.github.com>
2020-09-18 01:58:43 +0530

    prev prog has no problem but i had ran it on jupyter so parts of the code was showing problem

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,14 +1,16 @@
-# import numpy as np
+# printing of the sudoku board to the console
+import numpy as np
+
 board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
+    [5,3,0,0,7,0,0,0,0],
+    [6,0,0,1,9,5,0,0,0],
+    [0,9,8,0,0,0,0,6,0],
+    [8,0,0,0,6,0,0,0,3],
+    [4,0,0,8,0,3,0,0,1],
+    [7,0,0,0,2,0,0,0,6],
+    [0,6,0,0,0,0,2,8,0],
+    [0,0,0,4,1,9,0,0,5],
+    [0,0,0,0,8,0,0,7,9]
 ]
 # print(np.matrix(board))
 # print(len(board))
@@ -17,14 +19,67 @@ def print_board(bo):
     for i in range(len(bo)):
         if i % 3 == 0 and  i !=0:
             print('- - - - - - - - - - -')
-        
+
         for  j in range(len(bo)):
             if j % 3 == 0 and j !=0:
-                print('|', end = ' ')
+                print('|', end=" ")
             if j == 8:
                 print(bo[i][j])
             else:
-                print(bo[i][j], end= ' ')
+                print(bo[i][j], end=" ")
+
+def find_empty(bo):
+    # empty = []
+    for x in range(len(bo)):
+        for y in range(len(bo)):
+            if bo[x][y] == 0:
+                return (x ,y)
+                return True
+
+    # to see the empty positions in the board
+    #             empty.append([i,j])
+    # return empty
                 
-                
+
+
+# print(find_empty(board))
+
+#print(find_empty(board))
+
+def valid(bo, x , y , num):
+    for i in range(len(bo)):
+        if bo[x][i] == num:
+            return False
+    for i in range(len(bo)):
+        if bo[i][y] == num:
+            return False
+
+    x0_box = (x//3)*3
+    y0_box = (y//3)*3
+    for i in range(0,3):
+        for j in range(0,3):
+            if bo[x0_box+i][y0_box+j] == num:
+                return False
+
+    return True
+
+#valid(board, 0, 2, 5 )
+
+def solve(bo):
+    found = find_empty(bo)
+    if not found:
+        return True
+    else:
+        x, y = found
+
+    for num in range(1,10):
+            if valid(bo,x, y, num):
+                bo[x][y] = num
+                if solve(bo):
+                    return True
+                    
+                bo[x][y]= 0
+    return
+    
+solve(board)
 print_board(board)

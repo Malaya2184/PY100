889f6824f619503b9d78af0d559fd817c9099e6e
Malaya2184 <malayakumarswain8@gmail.com>
2020-11-26 19:24:38 +0530

    searching of a node method added

--- data structure/allinone.py
+++ data structure/allinone.py
@@ -1,6 +1,6 @@
 # A single node of a singly linked list
 class Node:
-  def __init__(self, data, next=None): 
+  def __init__(self, data, next=None):
     self.data = data
     self.next = next
 
@@ -44,11 +44,29 @@ class LinkedList:
               previous = current
               current = current.next
         previous.next = None
+# searching of an element in the linked list
+  def searchnode(self, value):
+    self.value = value
+    if self.head is None:
+          print("There is nothing to search in this linked list")
+    else:
+          current = self.head
+          while current.data != value:
+                if current.next is None:
+                      print("not found")
+                      break
+                else:
+                      current= current.next
+          if current.data == value:
+                print("yepee found it")
+
         
 # Singly Linked List with insertion and print methods
 
 L = LinkedList()
 L.printLL()
+# searching of a node when there is no node
+L.searchnode(4)
 # deletion of a node from the linked list where there is no node
 L.deletefirstnode()
 L.printLL()
@@ -62,5 +80,11 @@ L.printLL()
 # deletion of the first node from the linkedlist
 L.deletefirstnode()
 L.printLL()
+# deletion of last node
 L.deletelastnode()
+L.printLL()
+# searching of a node in the linked list
+L.searchnode(4)
+L.insert(15)
+L.searchnode(15)
 L.printLL()

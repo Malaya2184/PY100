b9704f4975b7dbc76130568272a74b2b4dad18b3
Malaya2184 <malayakumarswain8@gmail.com>
2020-11-29 19:10:45 +0530

    insertion shot program added

--- data structure/sorting/sorting.py
+++ data structure/sorting/sorting.py
@@ -1,4 +1,5 @@
 # selection shot algorithm
+
 def selection_sort(list):
     for i in range(len(list)):
         min_index = i
@@ -9,12 +10,15 @@ def selection_sort(list):
         list[i],list[min_index]=list[min_index], list[i]
     return list
 
+
+# TODO: It is not an efficent program for the solution
 # quickshot algorithm
 def quick_short(list):
     length_of_list = len(list)
     if length_of_list <= 1:
         return list
     else:
+    # here the pop method automatically poped out the last element of the list and return the list to the functiom
         pivot = list.pop()
         
     pivot_grearer =[]
@@ -25,6 +29,7 @@ def quick_short(list):
         else:
             pivot_smaller.append(i)
     return quick_short(pivot_smaller) + [pivot] + quick_short(pivot_grearer)
+
 
 # Bubble sort algorithm
 def bubble_sort(list):
@@ -41,8 +46,19 @@ def bubble_sort(list):
 
     return list
 
+
+# insertion shot algorithm
+def insertion_sort(list):
+    for i in range(1, len(list)):
+        while list[i-1] > list[i] and i>0:
+            list[i-1],list[i] = list[i], list[i-1]
+            i = i-1
+    
+    return list
+
 #####################################################################
 # list = [3,4,5,1,2,3,0,7,8,90,67,31,2,5,567]
 print(selection_sort([3,4,5,1,2,3,0,7,8,90,67,31,2,5,567]))
 print(quick_short([3,4,5,1,2,3,0,7,8,90,67,31,2,5,567]))
 print(bubble_sort([3,4,5,1,2,3,0,7,8,90,67,31,2,5,567]))
+print(insertion_sort([3,4,5,1,2,3,0,7,8,90,67,31,2,5,567]))

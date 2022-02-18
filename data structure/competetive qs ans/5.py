8f6106e7f67d4c62410c9a74b5c5774d0cd7c176
Malaya2184 <malayakumarswain8@gmail.com>
2022-02-18 18:57:50 +0530

    5 solved

---
+++ data structure/competetive qs ans/5.py
@@ -0,0 +1,23 @@
+
+from enum import unique
+
+
+myarr = [1,2,3,4,5,6,7,8,9]
+
+def isunique (myarr):
+    unique =0
+    for i in range (len(myarr)):
+        for j in range(i+1, len(myarr)):
+            if(j<len(myarr)):
+                if myarr[i]== myarr[j]:
+                    print("array is not unique")
+                    unique = 1
+                    break
+                else:
+                    if(i == len(myarr)-2) and unique == 0:
+                        print("array is unique")
+                        break
+                    continue  
+    
+
+isunique(myarr)

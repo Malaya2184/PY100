f5c9949cc7eb8284e57d6255ba562a6eee58fd12
Malaya2184 <malayakumarswain8@gmail.com>
2022-05-13 16:28:31 +0530

    best way to do heap sort

--- Practice_interview/linkedlist.py
+++ Practice_interview/linkedlist.py
@@ -1,5 +1,3 @@
-from locale import currency
-
 
 class node :
     def __init__(self, data, next = None):
@@ -28,9 +26,61 @@ class linkedList:
             print(current.data,"------>", end="")
             current = current.next
         print(None)
+        
+    def insertmultipleNodes(self,arr):
+        for i in arr:
+            self.insertnode(i)
+            
+    def deleteLastNode(self):
+        current=self.head
+        while current.next is not None:
+            prev = current
+            current = current.next
+        prev.next = None
+        
+    def sortLinkedList(self):
+        current = self.head
+        arr=[]
+        while  current is not  None:
+            arr.append(current.data)
+            current=current.next
+        
+        arr.sort()
+        self.head = None
+        self.insertmultipleNodes(arr)
+    
+    def deleteFirstNode(self):
+        current = self.head
+        
+        if current.next == None:
+            self.head = None
+        else:
+            self.head = current.next
+            
+    def searchElementInLinkedlist(self,data):
+        current = self.head
+        count = 1
+        while current is not None:
+            if current.data == data:
+                print(data, " first found at", count, "position")
+                break
+            else:
+                current = current.next
+                count+=1
 
 l = linkedList()
 l.insertnode(5)
 l.printLinkedList()
 l.insertnode(9)
 l.printLinkedList()
+l.insertmultipleNodes([3,5,7,9,10])
+l.printLinkedList()
+l.deleteLastNode()
+l.printLinkedList()
+l.sortLinkedList()
+l.printLinkedList()
+l.deleteFirstNode()
+l.printLinkedList()
+l.searchElementInLinkedlist(9)
+l.searchElementInLinkedlist(7)
+l.searchElementInLinkedlist(5)

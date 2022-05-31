7f074d12a822c852259acc94014c456591da01d3
Malaya2184 <malayakumarswain8@gmail.com>
2022-05-31 13:16:08 +0530

    ll sorting done

--- Practice_interview/practice_ll.py
+++ Practice_interview/practice_ll.py
@@ -1,6 +1,3 @@
-from requests import delete
-
-
 class node:
     def __init__(self, data, next = None):
         self.data = data
@@ -30,15 +27,65 @@ class linkedList:
                 current = current.next
             print(current.data,'--->',current.next)
     def deleteLastNode(self):
+        if self.head is None:
+            return(print("no linkedlist present"))
+        elif self.head.next is None:
+            self.head = None
+            return(print('there was a single node in LL which has deleted'))
+        else:
+            current = self.head
+            while current.next is not None:
+                prev = current
+                current = current.next
+            prev.next = None
+    def deleteFirstNode(self):
+        if self.head is None:
+            return(print("no linkedlist present"))
+        elif self.head.next is None:
+            self.head = None
+            return(print('there was a single node in LL which has deleted'))
+        else:
+            current = self.head
+            if current.next is not None:
+                self.head = current.next
+    def lengthOfLL(self):
         current = self.head
+        count = 0
+        if  current is None:
+            return 'linked list has nt created yet unable to find length'
+        else:
+            while current.next is not None:
+                count+=1
+                current = current.next
+            return(print('length of linked list is :', count+1))
+    def sortLinkedList(self):
+ 
+        current = self.head
+        myarr = []
         while current.next is not None:
-            prev = current
+            myarr.append(current.data)
             current = current.next
-        prev.next = None
+        myarr.append(current.data)
+        myarr.sort()
+        self.head= None
+        self.insertMultipleNode(myarr)    
+
+            
 l = linkedList()
 l.insertNode(5)
+l.deleteLastNode()
 l.printLL()
 l.insertMultipleNode([7,8,15])
 l.printLL()
 l.deleteLastNode()
 l.printLL()
+l.lengthOfLL()
+l.deleteFirstNode()
+l.printLL()
+l.lengthOfLL()
+l.insertMultipleNode([25,3,5])
+l.printLL()
+l.lengthOfLL()
+l.sortLinkedList()
+l.printLL()
+l.lengthOfLL()
